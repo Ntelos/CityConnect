938e05c9ac2777d6eda65bd1abb0e57833ad684e
Panteleimon Kampouris <35311350+Ntelos@users.noreply.github.com>
2021-03-15 19:17:36 +0200

    Update ProfileActivity.java

--- app/src/main/java/com/example/cityconnect/ProfileActivity.java
+++ app/src/main/java/com/example/cityconnect/ProfileActivity.java
@@ -2,13 +2,41 @@ package com.example.cityconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class ProfileActivity extends AppCompatActivity {
+
+    TextView email;
+    Button logout_button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        logout_button = findViewById(R.id.logout_button);
+        email = findViewById(R.id.profile_email);
+
+        GoogleSignInAccount signInAccount = GoogleSignIn.getLastSignedInAccount(this);
+        if(signInAccount!=null){
+            email.setText(signInAccount.getEmail());
+        }
+
+        logout_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                //go back to sign in layout
+                finish();
+            }
+        });
     }
 }
